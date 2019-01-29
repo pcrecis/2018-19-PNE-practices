1005f929b6585551a11cfa42ba366c1f0becb840
pcrecis <pcrecis00@gmail.com>
2019-01-29 16:36:40 +0100

    Final try of the exercises 4 of session 2

---
+++ session-2/dna_count.py
@@ -0,0 +1,9 @@
+
+seq = input("Introduce a sequence of DNA: ")
+seq = seq.upper()
+print("The total length of the sequence",seq , "is",len(seq))
+print("The number of bases 'A' is: ", seq.count('A'))
+print("The number of bases 'C' is: ", seq.count('C'))
+print("The number of bases 'T' is: ", seq.count('T'))
+print("The number of bases 'G' is: ", seq.count('G'))
+
