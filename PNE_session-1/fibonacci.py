2a25eea32432baa4e38a4c6e86dcb1e55ff482c2
pcrecis <pcrecis00@gmail.com>
2019-01-29 16:51:35 +0100

    Final try of the exercise 5 of session 1

--- PNE_session-1/fibonacci.py
+++ PNE_session-1/fibonacci.py
@@ -1,2 +1,11 @@
-def fibonacci(n)
-    
+print("Adding the fibonacci terms from 1 to n. In this case the n parameter is a constant: '100'")
+
+def fib(n):
+    a, b = 0,1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+    return
+
+fib(100)
