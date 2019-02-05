14dca9807d9a7172093d25b7de137e8bd604e45a
pcrecis <pcrecis00@gmail.com>
2019-02-05 15:39:48 +0100

    Final try of the exercise 6 of session 1

--- P0/fibonacci_sum.py
+++ P0/fibonacci_sum.py
@@ -1,12 +1,11 @@
-def fib(n):
-    a, b = 0,1
-    while a < n:
-        print(a, end=' ')
-        a, b = b, a+b
-    print()
-    return
-
-#--- Main program
 print("Adding the fibonacci terms from 1 to n.")
 n = int(input("Please, introduce n: "))
-fib(n)
+
+def fibonacci(n):
+    counter = 0
+    a, b = 0,1
+    for i in range(n-1):
+        a, b = b,a+b
+        counter += a
+    print(counter)
+fibonacci(n)
