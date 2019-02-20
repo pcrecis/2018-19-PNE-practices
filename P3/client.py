488be8434163f99243cee0538bf985c50f9ebc76
pcrecis <pcrecis00@gmail.com>
2019-02-20 19:00:26 +0100

    client of Practice 3

---
+++ P3/client.py
@@ -0,0 +1,36 @@
+import socket
+import termcolor
+
+# SERVER IP, PORT
+PORT = 8094
+IP = "212.128.253.105"
+
+while True:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # establish the connection to the Server (IP, PORT)
+    s.connect((IP, PORT))
+
+    set = ""
+    while True:
+        msg = (input("Introduce: ") + "\n")
+        if msg and msg.strip():
+            set += msg
+        else:
+            break
+
+    if len(set) == 0:
+        set = " "
+
+    s.send(str.encode(set))
+    print(set)
+
+    # Send the request message to the server
+
+    # Receive the servers respoinse
+    response = s.recv(2048).decode('utf-8')
+
+    # Print the server's response
+    termcolor.cprint("Response: {}".format(response), 'red')
+
+    s.close()
