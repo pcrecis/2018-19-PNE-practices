65ff5d49254d2d9d6322a7e3f27936190affa213
pcrecis <pcrecis00@gmail.com>
2019-04-01 11:52:22 +0200

    client of practice 7 updated and finished

--- P7/client.py
+++ P7/client.py
@@ -25,17 +25,17 @@ print("Response received!: {} {}\n".format(r1.status, r1.reason))
 # -- Read the response's body
 data1 = r1.read().decode("utf-8")
 
-print("CONTENT: ")
-
 # -- Create a variable with the data,
 # -- form the JSON received
 FRAT1_data = json.loads(data1)
 
-seq = Seq(FRAT1_data["seq"])
+seq = FRAT1_data["seq"]
 
 termcolor.cprint("Type of molecule: {}".format(FRAT1_data['molecule']), 'yellow')
 termcolor.cprint("The sequence for the FRAT1 gene is: {}".format(seq), 'green')
 print("")
+
+seq = Seq(seq)
 
 # Total length of the sequence:
 tl = seq.len()
@@ -44,19 +44,19 @@ counter_A = seq.count_bases('A')
 counter_C = seq.count_bases('C')
 counter_T = seq.count_bases('T')
 counter_G = seq.count_bases('G')
+num_max = max(counter_A, counter_C, counter_T, counter_G)
 
-dict_bases = {'A':'counter_A', 'C':'counter_C', 'T':'counter_T', 'G':'counter_G'}
-num_max = max(dict_bases.keys())
+dict_bases = dict(zip(['A', 'C', 'T', 'G'],[counter_A, counter_C, counter_T, counter_G]))
 
-for i in dict_bases.keys():
-    if dict_bases[i] == num_max:
-        b_popular = i
+for key, value in dict_bases.items():
+    if value == num_max:
+        b_popular = key
 
 termcolor.cprint("  CONTENT: ", 'blue')
 
 print("There are {} bases in the FRAT1 gene.".format(tl))
 print("Of the ({}) bases of the FRAT1 gene, {} are T bases.".format(tl, counter_T))
-print("The most popular base in the FRAT1 gene is: {} and its percentage: {}%".format(b_popular, seq.perc(b_popular)))
+print("The most popular base in the FRAT1 gene is: {}, and its percentage: {}%".format(b_popular, seq.perc(b_popular)))
 
 # Percentage of all bases:
 for b in dict_bases:
